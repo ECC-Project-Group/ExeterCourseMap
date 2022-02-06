3c3104708127db3eaacde43a9ed46d49d1c1efa2
Knoddy <cow2cow4cow@gmail.com>
2022-02-06 13:11:14 -0500

    Remove default styles and implement landing page

---
+++ components/callToActionButton.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import { BsChevronRight } from 'react-icons/bs';
+
+const CTAButton = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: JSX.Element | string;
+}) => {
+  return (
+    <Link href={href} passHref={true}>
+      <button className="group flex flex-row rounded-md bg-exeter p-3 font-display text-xl font-semibold text-white transition-all hover:bg-red-700 ">
+        {children}{' '}
+        <BsChevronRight className="my-auto mx-2 transition-all group-hover:translate-x-2" />
+      </button>
+    </Link>
+  );
+};
+
+export default CTAButton;
---
+++ components/header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+
+const HeaderNavItem = ({ name, href }: { name: string; href: string }) => {
+  return (
+    <li className="pl-4">
+      <Link href={href}>
+        <a>{name}</a>
+      </Link>
+    </li>
+  );
+};
+
+const Header = () => {
+  return (
+    <header className="absolute flex w-full flex-row justify-between bg-none py-4 px-8 lg:px-40">
+      <Link href="/" passHref={true}>
+        <a className="font-display text-lg font-black text-white">
+          EXETER COURSE MAP
+        </a>
+      </Link>
+
+      <ul className="flex flex-row justify-start font-display text-white">
+        <HeaderNavItem name="Courses" href="/courses" />
+        <HeaderNavItem name="Map" href="/map" />
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
