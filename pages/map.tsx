78da806e42be3a6afbff18e4211166dbc6df5729
Knoddy <cow2cow4cow@gmail.com>
2022-02-09 00:46:31 -0500

    Add placeholder map page

---
+++ pages/map.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Courses: NextPage = () => {
+  return (
+    <div>
+      <div className="bg-exeter px-8 py-7 lg:px-40"></div>
+    </div>
+  );
+};
+
+export default Courses;
