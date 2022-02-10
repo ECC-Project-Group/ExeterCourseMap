af7ddad7ea5012ad8ed77bcd75c002700d16ca91
Eric Li <mastermind@Masternode-2.local>
2022-02-10 16:08:14 -0500

    Added authentication... Work required on styling

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 const HeaderNavItem = ({ name, href }: { name: string; href: string }) => {
   return (
@@ -11,6 +12,8 @@ const HeaderNavItem = ({ name, href }: { name: string; href: string }) => {
 };
 
 const Header = () => {
+  const { data: session, status } = useSession();
+
   return (
     <header className="absolute flex w-full flex-row justify-between bg-none py-4 px-8 lg:px-40">
       <Link href="/" passHref={true}>
@@ -22,7 +25,45 @@ const Header = () => {
       <ul className="flex flex-row justify-start font-display text-white">
         <HeaderNavItem name="Courses" href="/courses" />
         <HeaderNavItem name="Map" href="/map" />
-        <HeaderNavItem name="Login" href="/login" />
+        <div>
+          {!session && (
+            <>
+              <span>You are not signed in</span>
+              <a
+                href={`/api/auth/signin`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn();
+                }}
+              >
+                Sign in
+              </a>
+            </>
+          )}
+          {session?.user && (
+            <>
+              {session.user.image && (
+                <span
+                  style={{ backgroundImage: `url('${session.user.image}')` }}
+                />
+              )}
+              <span>
+                <small>Signed in as</small>
+                <br />
+                <strong>{session.user.email ?? session.user.name}</strong>
+              </span>
+              <a
+                href={`/api/auth/signout`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  signOut();
+                }}
+              >
+                Sign out
+              </a>
+            </>
+          )}
+        </div>
       </ul>
     </header>
   );
