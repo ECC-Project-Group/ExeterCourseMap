20e90399104f91d074220b32d9e7d98399ef3a22
Cowation <cow2cow4cow@gmail.com>
2022-09-10 23:07:15 -0400

    Add new logo icons and html tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,34 +33,98 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   // <Component /> represents the actual page content
   return (
-    <SessionProvider session={pageProps.session} refetchInterval={0}>
-      <ThemeProvider attribute="class">
-        <div className="min-h-[calc(100vh-316px)]">
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
-          `}
-          </Script>
-          <Head>
-            <title>Exeter Course Map</title>
-            <meta
-              title="description"
-              content="The better way to pick courses"
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="57x57" href="/favicon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/favicon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/favicon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/favicon-76x76.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/favicon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/favicon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/favicon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/favicon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon-192x192.png"
+        />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/favicon-144x144.png" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff"></meta>
+      </Head>
+      <SessionProvider session={pageProps.session} refetchInterval={0}>
+        <ThemeProvider attribute="class">
+          <div className="min-h-[calc(100vh-316px)]">
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+              strategy="afterInteractive"
             />
-          </Head>
-          <Header />
-          <Component {...pageProps} key={router.route} />
-        </div>
-        <Footer />
-      </ThemeProvider>
-    </SessionProvider>
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+            `}
+            </Script>
+            <Head>
+              <title>Exeter Course Map</title>
+              <meta
+                title="description"
+                content="The better way to pick courses"
+              />
+            </Head>
+            <Header />
+            <Component {...pageProps} key={router.route} />
+          </div>
+          <Footer />
+        </ThemeProvider>
+      </SessionProvider>
+    </>
   );
 }
 
